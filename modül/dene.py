e9548ce292e12f71f28bed4dbac6e621a2dabf3c
Burcu Masat <63057571+brcmst@users.noreply.github.com>
2021-02-07 20:47:30 +0300

    Add files via upload

---
+++ modül/dene.py
@@ -0,0 +1,5 @@
+import dort_islem_modul as islem
+
+print(islem.topla(3,5))
+print(islem.cikarma(7,5))
+print(islem.islemler)
---
+++ modül/dort_islem_modul.py
@@ -0,0 +1,21 @@
+islemler = ["toplama","çıkarma","çarpma","bölme"]
+
+def topla(a,b):
+    """
+    2 değer toplamını döndürür.
+    """
+    return a + b
+
+def cikarma(a,b):
+    """
+    2 değer farkını döndürür.
+    """
+    return a - b
+
+def carpma(a,b):
+    """
+    2 değer çarpımını döndürür.
+    """
+    return a * b
+
+    
