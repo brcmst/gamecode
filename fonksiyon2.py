d5d19cf7eef6ead98c76051cd1953e97ea3e12cc
Burcu Masat <63057571+brcmst@users.noreply.github.com>
2021-02-12 21:07:22 +0300

    Delete fonksiyon2.py

--- fonksiyon2.py
+++
@@ -1,9 +0,0 @@
-#parametreli fonk. ile isim ile merhaba ...isim.. yazdır
-
-isim = input("isim: ") #kullanıcıdan alınan deger
-
-def myFonk(a):         #fonksiyon içinde belirlenen argüman adı önemsizdir
-    print("merhaba ",a)
-    
-myFonk(isim)           #fonksiyonu kullanıcıdan aldığımız değeri parametre olarak gönderdik
-
